60ffebbf0e273e38c2b2809fb6fcbff49d14e246
naveen.rao <naveen.rao@appdynamics.com>
2020-02-17 20:58:09 -0800

    add sleep back.

--- src/main/java/com/appdynamics/analytics/demo/LoadRunner.java
+++ src/main/java/com/appdynamics/analytics/demo/LoadRunner.java
@@ -31,8 +31,11 @@ public class LoadRunner implements Runnable {
     public void run() {
         while (true) {
             callPortalAuthenticate(portalUrl,portalPort);
+            sleep();
             callPortalSubmitApplication(portalUrl, portalPort);
+            sleep();
             callProcessorCreditCheck(processorUrl, processorPort);
+            sleep();
             callProcessorUnderWrite(processorUrl, processorPort);
             sleep();
         }
